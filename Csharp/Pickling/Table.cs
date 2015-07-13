c7381c1562100963f03a70b177170092fbccebe2
lucabordo <lucabordo@hotmail.co.uk>
2015-07-13 08:42:03 +0100

    Sketched the Table logic for single-segment approach

--- Csharp/Pickling/Table.cs
+++ Csharp/Pickling/Table.cs
@@ -11,44 +11,66 @@ using Common;
 
 namespace Pickling
 {
+    /// <summary>
+    /// A contiguous, random access collection of elements of a certain Schema,
+    /// transparently serialized into two ByteContainers.
+    /// </summary>
     public class Table<T> : IDisposable
     {
-        #region Fields
+        #region Provided fields
 
-        // Provided
-
-        private readonly ByteContainer fixedContainer;
-        private readonly ByteContainer dynamicContainer;
+        /// <summary>
+        /// The schema of the contained elements
+        /// </summary>
         private readonly Schema<T> schema;
 
-        // Allocated internally
+        /// <summary>
+        /// A container that contains a number of contiguous elements
+        /// </summary>
+        private readonly ByteContainer data;
 
-        // Cached 
+        /// <summary>
+        /// The list of starting point and sizes in the data container
+        /// </summary>
+        private readonly ByteContainer index;
 
-        private int fixedEncodingSize;
+        private static readonly int IndexEntryEncodingSize = Encoding.EncodingSizeForLong + Encoding.EncodingSizeForInt;
 
-//        private readonly ByteSegmentReadView reusedReader;
-//        private readonly ByteSegmentWriteView reusedWriter;
+        #endregion
+
+        #region Cached data
+
+        /// <summary>
+        /// If the schema is fixed size we keep its size
+        /// </summary>
+        private Nullable<int> FixedSize;
 
         #endregion
 
         #region Construction and Disposal
 
-        public Table(ByteContainer fixedContainer, ByteContainer dynamicContainer, Schema<T> schema)
+        public Table(Schema<T> schema, ByteContainer fixedContainer, ByteContainer dynamicContainer)
         {
-            this.fixedContainer = fixedContainer;
-            this.dynamicContainer = dynamicContainer;
+            this.index = fixedContainer;
+            this.data = dynamicContainer;
             this.schema = schema;
-//            fixedEncodingSize = schema.GetFixedSize();
 
-  //          if (fixedContainer.Count % fixedEncodingSize != 0)
-    //            throw new ArgumentException("Fixed storage should be of size multiple of the schema fixed size");
+            FixedSize = (schema.IsFixedSize)
+                ? FixedSize = schema.GetDynamicSize(default(T))
+                : null;
+
+
+            ReadIndexBuffer = new ByteBuffer(IndexEntryEncodingSize);
+            ReadIndexSegment = ReadIndexBuffer.GetReadView(0, IndexEntryEncodingSize);
+
+            //          if (fixedContainer.Count % fixedEncodingSize != 0)
+            //            throw new ArgumentException("Fixed storage should be of size multiple of the schema fixed size");
         }
 
         public void Dispose()
         {
-            fixedContainer.Dispose();
-            dynamicContainer.Dispose();
+            index.Dispose();
+            data.Dispose();
         }
 
         #endregion
@@ -59,58 +81,105 @@ namespace Pickling
         {
             get
             {
-                Contract.Invariant(fixedContainer.Count % fixedEncodingSize == 0);
-                return fixedContainer.Count / fixedEncodingSize;
+                //                Contract.Invariant(index.Count % fixedEncodingSize == 0);
+                //              return index.Count / fixedEncodingSize;
+
+                return 0;
             }
         }
 
 
-        // Fields for use only in the Read method
-        private readonly ByteBuffer fixedReadBuffer = new ByteBuffer();
-        private readonly ByteBuffer dynamicReadBuffer = new ByteBuffer();
+        private struct IndexEntry
+        {
+            /// <summary>
+            /// Start of object encoding in data storage
+            /// </summary>
+            public long Start;
+
+            /// <summary>
+            /// Length of object encoding in data storage
+            /// </summary>
+            public int Length;
+        }
+
+        // Fields for use only in the ReadIndex method
+        private readonly ByteBuffer ReadIndexBuffer = new ByteBuffer();
+        private ByteSegmentReadView ReadIndexSegment;
+
+        private IndexEntry ReadIndex(long position)
+        {
+            ReadIndexBuffer.Resize(Encoding.EncodingSizeForLong + Encoding.EncodingSizeForInt); // TODO: Once
+            ReadIndexSegment = ReadIndexBuffer.GetReadView(0, IndexEntryEncodingSize); // TODO: REINITIALIZE INSTEAD 
+            index.Read(ReadIndexSegment, position * IndexEntryEncodingSize);
+
+            return new IndexEntry
+            {
+                Start = Encoding.ReadLong(ReadIndexSegment),
+                Length = Encoding.ReadInt(ReadIndexSegment)
+            };
+        }
+
+        // Fields for use only in the WriteIndex method
+        private readonly ByteBuffer WriteIndexBuffer = new ByteBuffer();
+        private ByteSegmentWriteView WriteIndexSegment;
+
+        private void WriteIndex(long position, IndexEntry entry)
+        {
+            WriteIndexBuffer.Resize(Encoding.EncodingSizeForLong + Encoding.EncodingSizeForInt); // TODO: Once
+            WriteIndexSegment = WriteIndexBuffer.GetWriteView(0, IndexEntryEncodingSize); // TODO: REINITIALIZE INSTEAD 
+            Encoding.WriteLong(WriteIndexSegment, entry.Start);
+            Encoding.WriteInt(WriteIndexSegment, entry.Length);
+            index.Write(WriteIndexSegment, position * IndexEntryEncodingSize);
+        }
+
+        private readonly ByteBuffer ReadDataBuffer = new ByteBuffer();
+        private ByteSegmentReadView ReadDataSegment;
+
+        private T ReadData(long start, int size)
+        {
+            ReadDataBuffer.Resize(size);
+            ReadDataSegment = ReadDataBuffer.GetReadView(0, size); // TODO: REINITIALIZE INSTEAD 
+            data.Read(ReadDataSegment, start);
+            return schema.Read(ReadDataSegment);
+        }
+
+        private readonly ByteBuffer WriteDataBuffer = new ByteBuffer();
+        private ByteSegmentWriteView WriteDataSegment;
+
+        private void WriteData(long position, T element, int size)
+        {
+            WriteDataBuffer.Resize(size);
+            WriteDataSegment = WriteDataBuffer.GetWriteView(0, size); // TODO: REINITIALIZE INSTEAD 
+            schema.Write(WriteDataSegment, element);
+        }
 
         public T Read(long position)
         {
-//            var dynamicSize = schema.GetDynamicSize();
-            fixedReadBuffer.Resize(fixedEncodingSize);
-            
-            var fixedReader = fixedReadBuffer.GetReadView(0, fixedEncodingSize);
-            //var dynamicReader = 
+            if (FixedSize != null)
+            {
+                // optimizations
+            }
 
-            return default(T);
-        }
-
-
-        // Fields for use only in the Write method
-        private readonly ByteBuffer fixedWriteBuffer = new ByteBuffer();
-        private readonly ByteBuffer dynamicWriteBuffer = new ByteBuffer();
-
-        // Start by this: APPEND THAT ALLOWS EFFICIENT READ.
-        public void Append(T element)
-        {
-            // Positions
-            long fixedStart = (fixedEncodingSize + Encoding.EncodingSizeForLong + Encoding.EncodingSizeForInt);
-            long end = dynamicContainer.Count;
-
-            // Encoding 
-            var dynamicEncodingSize = schema.GetDynamicSize(element);
-
-            fixedWriteBuffer.Resize(fixedEncodingSize + Encoding.EncodingSizeForLong + Encoding.EncodingSizeForInt);
-            dynamicWriteBuffer.Resize(dynamicEncodingSize);
-
-            var fixedWriter = fixedWriteBuffer.GetWriteView(0, fixedEncodingSize);
-            var dynamicWriter = dynamicWriteBuffer.GetWriteView(0, dynamicEncodingSize);
-
-//            schema.Write(fixedWriter, dynamicWriter, element);
-
-            // Emplace the buffers into the container
-            fixedContainer.Write(fixedWriter, 0);
-            dynamicContainer.Write(dynamicWriter, 0);
+            IndexEntry index = ReadIndex(position);
+            return ReadData(index.Start, index.Length);
         }
 
         public void Write(long position, T element)
         {
-            // Even here we need to locate the right STARTING POINT IN dynamic storage
+            if (FixedSize != null)
+            {
+                // TODO: optimizations
+            }
+            
+            long start = data.Count;
+            int size = schema.GetDynamicSize(element);
+
+            WriteIndex(position, new IndexEntry { Start = start, Length = size });
+            WriteData(start, element, size);
+
+            // TODO: Some assertions here!!
+
+            // TODO: Keep track of fragmentation. This would require an index read before write...
         }
 
         #endregion
